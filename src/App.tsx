0e6eb94f27b6bf43fbe14e81e114ddb62d067782
Filipe Dias <filipediascontato@gmail.com>
2022-12-19 18:58:48 -0300

    Creates the Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Home } from "./pages/Home";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -7,7 +8,7 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       
-      <h1>DT Money</h1>
+      <Home />
     </ThemeProvider>
   )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background: ${props => props.theme.color["gray-900"]};
+  padding: 2.5rem 0 7.5rem;
+`
+
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 70rem;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+`
+
+export const NewTransactionButton = styled.button`
+  height: 3.125rem;
+  padding: 0.75rem 1.25rem;
+  background: ${props => props.theme.color["green-500"]};
+  border: 0;
+  border-radius: 6px;
+  color: ${props => props.theme.color.white};
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.1s;
+
+  &:hover {
+    background: ${props => props.theme.color["green-700"]};
+  }
+`
