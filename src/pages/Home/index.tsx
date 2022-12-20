2be2617ad803a79062cf9f447db7262f3fe2df22
Filipe Dias <filipediascontato@gmail.com>
2022-12-19 23:31:57 -0300

    Implements the TransactionsTable

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,8 @@
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
@@ -5,8 +10,31 @@ export function Home() {
   return (
     <div>
       <Header />
-
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/12/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Bar</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 200,00</PriceHighlight>
+              </td>
+              <td>Lazer</td>
+              <td>13/12/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const TransactionsContainer = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 70rem;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${(props) => props.theme.color['gray-700']};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+
+interface PriceHighlightProps {
+  variant: 'income' | 'outcome'
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${(props) =>
+    props.variant === 'income'
+      ? props.theme.color['green-300']
+      : props.theme.color['red-300']};
+`
