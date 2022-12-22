093691fa003d8e30c6c728a839478f2085ac5bc9
Filipe Dias <filipediascontato@gmail.com>
2022-12-21 22:05:27 -0300

    Finishes the NewTransactionModal component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,12 @@
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import * as Dialog from '@radix-ui/react-dialog'
-import { Content, Overlay } from './styles'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionTypeButton,
+  TransactionTypeContainer,
+} from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -9,15 +16,29 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
 
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
         <form>
           <input type="text" placeholder="Descrição" required />
           <input type="text" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
 
+          <TransactionTypeContainer>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entradas
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saídas
+            </TransactionTypeButton>
+          </TransactionTypeContainer>
+
           <button type="submit">Cadastrar</button>
         </form>
-
-        <Dialog.Close />
       </Content>
     </Dialog.Portal>
   )
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import * as Dialog from '@radix-ui/react-dialog'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -53,5 +54,66 @@ export const Content = styled(Dialog.Content)`
         background: ${(props) => props.theme.color['green-700']};
       }
     }
+  }
+`
+
+export const CloseButton = styled(Dialog.Close)`
+  position: absolute;
+  background: transparent;
+  border: 0;
+  top: 1.5rem;
+  right: 1.5rem;
+  color: ${(props) => props.theme.color['gray-500']};
+  line-height: 0;
+  cursor: pointer;
+`
+
+export const TransactionTypeContainer = styled(RadioGroup.Root)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`
+
+interface TransactionTypeButtonProps extends RadioGroup.RadioGroupItemProps {
+  variant: 'income' | 'outcome'
+}
+
+export const TransactionTypeButton = styled(
+  RadioGroup.Item,
+)<TransactionTypeButtonProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  padding: 1rem;
+  background: ${(props) => props.theme.color['gray-700']};
+  border: 0;
+  border-radius: 6px;
+  color: ${(props) => props.theme.color['gray-300']};
+  transition: background-color 0.1s;
+  cursor: pointer;
+
+  &[data-state='unchecked']:hover {
+    background: ${(props) => props.theme.color['gray-600']};
+  }
+
+  &[data-state='checked'] {
+    color: ${(props) => props.theme.color.white};
+    background: ${(props) =>
+      props.variant === 'income'
+        ? props.theme.color['green-500']
+        : props.theme.color['red-500']};
+
+    svg {
+      color: ${(props) => props.theme.color.white};
+    }
+  }
+
+  svg {
+    color: ${(props) =>
+      props.variant === 'income'
+        ? props.theme.color['green-300']
+        : props.theme.color['red-300']};
   }
 `
